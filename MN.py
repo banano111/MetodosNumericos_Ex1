cf799d39f55f52af9fc45a6c49da5da8c408617c
Alan Arturo Pedraza Luna <alanbanano@gmail.com>
2020-03-11 16:24:12 -0600

    Cambiando a un solo archivo externo

---
+++ MN.py
@@ -0,0 +1,78 @@
+import sympy
+from random import randint
+import base64
+import io
+
+from sympy import *
+from numpy import *
+import matplotlib.pyplot as plt
+from sympy.parsing.sympy_parser import parse_expr
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+plt.switch_backend('agg')
+
+x, y = sympy.symbols('x y')
+
+def Graficacion(fx):
+    y = fx
+    x = linspace(-5,5,100)
+    y = eval(y)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.spines['left'].set_position('zero')
+    ax.spines['right'].set_color('none')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['top'].set_color('none')
+    ax.spines['left'].set_smart_bounds(True)
+    ax.spines['bottom'].set_smart_bounds(True)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    plt.grid(alpha=.4,linestyle='--')
+
+    plt.plot(x,y,'g')
+
+    # Convert plot to PNG image
+    pngImage = io.BytesIO()
+    FigureCanvas(fig).print_png(pngImage)
+    
+    # Encode PNG image to base64 string
+    pngImageB64String = "data:image/png;base64,"
+    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+    return pngImageB64String
+
+def Newton(valor):
+    
+    fx = parse_expr(valor)
+    
+    resultados = []
+    ER = 100
+    ban = 0
+    aleatorio = 2
+    dx = sympy.diff(fx,x)
+
+    while ER > 0:
+        ban = ban + 1
+        arriba = fx.subs(x,aleatorio)
+        abajo = dx.subs(x,aleatorio)    
+            
+        vx = aleatorio - float(arriba/abajo)
+        vx = round(vx,4)
+        VA = float((vx-aleatorio)/vx)
+        VA = round(VA,4)
+        VA = abs(VA)
+        ER = VA * 100
+        ER = round(ER,4)        
+
+        resultados.append(["Caso "+str(ban),vx,str(ER)+"%"])
+
+        aleatorio = vx
+            
+        if ban > 10:
+            break
+
+    return resultados
--- MN_Examen1_Flask.py
+++ MN_Examen1_Flask.py
@@ -1,84 +1,9 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 
-import sympy
-from random import randint
-import base64
-import io
+import MN as mn
 
-from sympy import *
-from numpy import *
-import matplotlib.pyplot as plt
-from sympy.parsing.sympy_parser import parse_expr
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-plt.switch_backend('agg')
 app = Flask(__name__, template_folder="templates")
-x, y = sympy.symbols('x y')
-
-def Graficacion():
-    
-    x = linspace(-5,5,100)
-    y = eval(request.form['fx'])
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.spines['left'].set_position('zero')
-    ax.spines['right'].set_color('none')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['top'].set_color('none')
-    ax.spines['left'].set_smart_bounds(True)
-    ax.spines['bottom'].set_smart_bounds(True)
-    ax.xaxis.set_ticks_position('bottom')
-    ax.yaxis.set_ticks_position('left')
-
-    plt.grid(alpha=.4,linestyle='--')
-
-    plt.plot(x,y,'g')
-
-    # Convert plot to PNG image
-    pngImage = io.BytesIO()
-    FigureCanvas(fig).print_png(pngImage)
-    
-    # Encode PNG image to base64 string
-    pngImageB64String = "data:image/png;base64,"
-    pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-
-    return pngImageB64String
-
-def Newton():
-    fx = parse_expr(request.form['fx'])
-    
-    resultados = []
-    ER = 100
-    ban = 0
-    aleatorio = 1
-    dx = sympy.diff(fx,x)
-
-    while ER > 0:
-        ban = ban + 1
-        arriba = fx.subs(x,aleatorio)
-        abajo = dx.subs(x,aleatorio)    
-            
-        vx = aleatorio - float(arriba/abajo)
-        vx = round(vx,4)
-        VA = float((vx-aleatorio)/vx)
-        VA = round(VA,4)
-        VA = abs(VA)
-        ER = VA * 100
-        ER = round(ER,4)        
-
-        resultados.append(["Caso "+str(ban),vx,str(ER)+"%"])
-
-        aleatorio = vx
-            
-        if ban > 10:
-            break
-    
-    print(resultados)
-    return resultados
     
 @app.route('/index')
 @app.route('/')
@@ -91,8 +16,9 @@ def MNR():
 
 @app.route('/MNR', methods=["POST"])
 def MNRes():
-    resultados = Newton()
-    pngImageB64String = Graficacion()
+    fx = request.form['fx']
+    resultados = mn.Newton(fx)
+    pngImageB64String = mn.Graficacion(fx)
     
     return render_template('derivadasres.html', resultados = resultados, image = pngImageB64String)
 
@@ -103,7 +29,8 @@ def graficas():
 @app.route('/graficas', methods=["POST"])
 def graficas_res():
 
-    pngImageB64String = Graficacion()
+    fx = request.form['fx']
+    pngImageB64String = mn.Graficacion(fx)
 
     return render_template('graficas.html', image = pngImageB64String)
 
